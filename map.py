c9a16cf14a5b3014abb99ed628ab563e9c65b300
Federico Paredes <federico@gospotcheck.com>
2019-10-14 20:43:47 -0600

    rename csv, clean up some data

--- map.py
+++ map.py
@@ -2,15 +2,20 @@
 
 
 # PULL IN DATA
-
-def pull_in_data():
+import csv
+def pull_in_data(filename):
+    with open(filename) as f:
+        lis = [line.split() for line in f]        # create a list of lists
+        for i, x in enumerate(lis):  
+            if i > 1 and i < 66:
+                print ("{0}".format(x))
     # put your code here to pull in the data
     # do some research to see how to pull in a CSV and use your resources if you need them
-
+pull_in_data("./data.csv")
 
 
 # CLEAN DATA
-def clean_data():
+# def clean_data():
     # put your code here to clean the data
 
 
