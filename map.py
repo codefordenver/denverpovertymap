884d620126c8332d3b2b0f44a4e585acb44b2ccb
Hannah Amundson <hannahkamundson@gmail.com>
2019-10-14 19:36:18 -0600

    Made first function

---
+++ map.py
@@ -0,0 +1,18 @@
+## PUT YOUR INPUT STATEMENTS HERE
+
+
+# PULL IN DATA
+
+def pull_in_data():
+    # put your code here to pull in the data
+    # do some research to see how to pull in a CSV and use your resources if you need them
+
+
+
+# CLEAN DATA
+def clean_data():
+    # put your code here to clean the data
+
+
+
+# if you want to test your f
